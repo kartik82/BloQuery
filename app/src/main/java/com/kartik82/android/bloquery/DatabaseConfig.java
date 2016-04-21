29ab507d9997fab7e609d8dc51984712258237d5
Kartik Shah <kartik82@gmail.com>
2016-04-21 20:29:29 +0300

    User Story 1 completed

---
+++ app/src/main/java/com/kartik82/android/bloquery/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.kartik82.android.bloquery;
+
+/**
+ * Created by Kartik on 21-Apr-16.
+ */
+public class DatabaseConfig {
+    public static final String FIREBASE_URL = "https://bloquery.firebaseio.com/";
+}
---
+++ app/src/main/java/com/kartik82/android/bloquery/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.kartik82.android.bloquery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Firebase.setAndroidContext(this);
+
+        // Login User
+        final EditText et_login_email = (EditText) findViewById(R.id.et_login_email);
+        final EditText et_login_password = (EditText) findViewById(R.id.et_login_password);
+        Button btn_login_login = (Button) findViewById(R.id.btn_login_login);
+
+        btn_login_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Firebase ref = new Firebase(DatabaseConfig.FIREBASE_URL);
+                    ref.authWithPassword(et_login_email.getText().toString(), et_login_password.getText().toString(), new Firebase.AuthResultHandler() {
+                        @Override
+                        public void onAuthenticated(AuthData authData) {
+                            Toast.makeText(getApplicationContext(), "Successfully Logged In!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onAuthenticationError(FirebaseError firebaseError) {
+                            Toast.makeText(getApplicationContext(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        // Show Register Activity
+        TextView tv_login_clickregister = (TextView) findViewById(R.id.tv_login_clickregister);
+
+        tv_login_clickregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+    }
+}
